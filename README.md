f0a5645ef047f77df125cde01f824117c92751a1
Charles Edeki <charlesedeki093@gmail.com>
2026-05-08 22:14:43 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# My First Pipeline
+
+A simple Python Calculator with an automated CI pipeline using GitHub.
+A push to main runs a test automatically.
+
+**Requirements:** Python 3.10 or higher
+
+
---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(10, 6) == 4
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+
+def test_divide():
+    assert divide(10, 5) == 10.0
